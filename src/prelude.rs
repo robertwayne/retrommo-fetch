05a7d4f5242ebee23f934eb9ca5e85d716e0b54c
Rob Wagner <rob@sombia.com>
2022-10-17 00:59:15 -0400

    Export Leaderboard in the prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,5 @@
 pub use crate::{
     get_leaderboard, get_leaderboard_page, get_online_players, get_online_players_full, get_player,
-    get_registered_player_count, get_top_players, LeaderboardEntry, LeaderboardPage, OnlineList,
-    Player,
+    get_registered_player_count, get_top_players, leaderboard::Leaderboard, LeaderboardEntry,
+    LeaderboardPage, OnlineList, Player,
 };
